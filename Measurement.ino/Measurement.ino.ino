f85501924df9da029d35e0cf073b4a35db86089a
Matthew Hennes <matthew.hennes@gmail.com>
2016-06-03 18:08:55 -0700

    Initial commit of Measurement.ino
    Added the Measurement.ino file, which will be the main controller
    program.

---
+++ Measurement.ino/Measurement.ino.ino
@@ -0,0 +1,13 @@
+// Top resistor: 248.4 k
+// Bottom resistor: 4.68 k
+// Req: 253.08 k
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
