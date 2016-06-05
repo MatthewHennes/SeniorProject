4c11044dd7bd976d8966edbe479a7f152f622226
Matthew Hennes <matthew.hennes@gmail.com>
2016-06-04 18:56:26 -0700

    Measurement working (for 3 batteries)
    somehow seems a bit low though

--- Measurement.ino/Measurement.ino.ino
+++ Measurement.ino/Measurement.ino.ino
@@ -14,7 +14,11 @@ const float R_MULTIPLIER = R_EQ / R_MEAS;
 const float V_MULTIPLIER = R_MULTIPLIER * STEP_SIZE;
 
 const int ADC_DISABLE = 6; //D7
-const int
+
+const int DEMUX1_ENABLE = 12; //D6
+const int DEMUX1_A = 8; //B4
+const int DEMUX1_B = 9; //B5
+const int DEMUX1_C = 10; //B6
 
 void setup()
 {
@@ -26,21 +30,45 @@ void setup()
   // Initialize ADC
   pinMode(ADC_DISABLE, OUTPUT);
   digitalWrite(ADC_DISABLE, HIGH);
+  
+  // Initialize Demux 1
+  pinMode(DEMUX1_ENABLE, OUTPUT);
+  digitalWrite(DEMUX1_ENABLE, LOW);
+  pinMode(DEMUX1_A, OUTPUT);
+  digitalWrite(DEMUX1_A, LOW);
+  pinMode(DEMUX1_B, OUTPUT);
+  digitalWrite(DEMUX1_B, LOW);
+  pinMode(DEMUX1_C, OUTPUT);
+  digitalWrite(DEMUX1_B, LOW);
 }
 
 void loop()
 {
   unsigned int ADCResult = 0;
   char value[ADC_VAL_BUFF_SIZE];
+  char battery[ADC_VAL_BUFF_SIZE];
+  char raw[ADC_VAL_BUFF_SIZE];
   float voltage = 0;
   
-  delay(1000);
-  ADCResult = takeADCSample();
-  voltage = ADCResult * V_MULTIPLIER;
-  Serial.write("Voltage: ");
-  String(voltage).toCharArray(value, ADC_VAL_BUFF_SIZE);
-  Serial.write(value);
+  for (int i = 0; i < 3; i++)
+  {
+    ADCResult = measureBattery(i);
+    voltage = ADCResult * V_MULTIPLIER;
+    Serial.write("Voltage ");
+    String(i).toCharArray(battery, ADC_VAL_BUFF_SIZE);
+    Serial.write(battery);
+    Serial.write(": ");
+    String(ADCResult).toCharArray(raw, ADC_VAL_BUFF_SIZE);
+    Serial.write(raw);
+    Serial.write(" = ");
+    String(voltage).toCharArray(value, ADC_VAL_BUFF_SIZE);
+    Serial.write(value);
+    Serial.write(" V\n");
+    delay(100);
+  }
   Serial.write("\n");
+  
+  delay(1000);
 }
 
 unsigned int takeADCSample()
@@ -58,4 +86,47 @@ unsigned int takeADCSample()
   digitalWrite(ADC_DISABLE, HIGH);
   
   return result;
+}
+
+unsigned int measureBattery(int battery)
+{
+  unsigned int ADC_Value;
+  
+  digitalWrite(DEMUX1_ENABLE, LOW);
+  digitalWrite(DEMUX1_A, LOW);
+  digitalWrite(DEMUX1_B, LOW);
+  digitalWrite(DEMUX1_C, LOW);
+  
+  switch(battery)
+  {
+    case 0:
+      digitalWrite(DEMUX1_ENABLE, HIGH);
+      break;
+      
+    case 1:
+      digitalWrite(DEMUX1_ENABLE, HIGH);
+      digitalWrite(DEMUX1_A, HIGH);
+      break;
+      
+    case 2:
+      digitalWrite(DEMUX1_ENABLE, HIGH);
+      digitalWrite(DEMUX1_B, HIGH);
+      break;
+    
+    default:
+      digitalWrite(DEMUX1_ENABLE, LOW);
+      digitalWrite(DEMUX1_A, LOW);
+      digitalWrite(DEMUX1_B, LOW);
+      digitalWrite(DEMUX1_C, LOW);
+      break;
+  }
+  
+  ADC_Value = takeADCSample();
+  
+  digitalWrite(DEMUX1_ENABLE, LOW);
+  digitalWrite(DEMUX1_A, LOW);
+  digitalWrite(DEMUX1_B, LOW);
+  digitalWrite(DEMUX1_C, LOW);
+  
+  return ADC_Value;
 }
